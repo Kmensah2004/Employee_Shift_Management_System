cabe1ce175ce29dc98fc2d87eb1ca17aaf342273
Kosi Mensah <kmensah@usf.edu>
2024-09-20 12:39:02 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management_system.js
@@ -0,0 +1,21 @@
+const employees = [ {  name: 'Dejalie', 
+    shifts: [ 
+    { day: 'Tuesday', hours: 7 }, 
+     { day: 'Thursday', hours: 5 }
+        ]
+    },{ 
+        name: 'Pamela', 
+        shifts: [
+            { day: 'Monday', hours: 8 }, 
+            { day: 'Wendnesday', hours: 7 }
+        ]
+    }, { 
+        name: 'Kosi', 
+        shifts: [{ day: 'Friday', hours: 6 }
+        ]
+    },{ 
+        name: 'Raymond', 
+        shifts: [ { day: 'Tuesday', hours: 4 }
+        ]
+    }
+];
